196d45e95b9a44197b8cea0fa5104d9b6c379f70
prxpostern <44132439+prxpostern@users.noreply.github.com>
2021-11-27 12:52:31 +0330

    Update download.py

--- helpers/download.py
+++ helpers/download.py
@@ -10,6 +10,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from helpers.progress import progress_func
 from helpers.tools import execute, clean_up
 from helpers.download_from_url import download_link
+from helpers.upload import upload_video
 
 DATA = {}
 
@@ -31,7 +32,7 @@ async def download_file(client, message):
 
     c_time = time.time()
 
-    download_location = await client.download_media(
+    dwld_loc = await client.download_media(
         message=media,
         progress=progress_func,
         progress_args=(
@@ -42,51 +43,21 @@ async def download_file(client, message):
     )
 
     await msg.edit_text("Processing your file....")
-
-    output = await execute(f"ffprobe -hide_banner -show_streams -print_format json '{download_location}'")
     
-    if not output:
-        await clean_up(download_location)
-        await msg.edit_text("Some Error Occured while Fetching Details...")
+    out_loc = os.path.basename(dwld_loc)
+    out_loc = os.path.splitext(out_loc)[0]
+    out_loc = str(out_loc) + "_320p.mp4"
+    
+    out, err, rcode, pid = await execute(f"ffmpeg -i \"{dwld_loc}\" -c:v libx264 -crf 20 -s 320*240 -c:a aac -af \"pan=stereo|c0=c01|c1=c1\" -ar 48000 -ab 96k \"{out_loc}\" -y")
+    if rcode != 0:
+        await message.edit_text(f"**Error Occured.**\n\n{err}")
+        print(err)
+        await clean_up(dwld_loc, out_loc)
         return
 
-    details = json.loads(output[0])
-    buttons = []
-    DATA[f"{message.chat.id}-{msg.message_id}"] = {}
-    for stream in details["streams"]:
-        mapping = stream["index"]
-        stream_name = stream["codec_name"]
-        stream_type = stream["codec_type"]
-        if stream_type in ("audio", "subtitle"):
-            pass
-        else:
-            continue
-        try: 
-            lang = stream["tags"]["language"]
-        except:
-            lang = mapping
-        
-        DATA[f"{message.chat.id}-{msg.message_id}"][int(mapping)] = {
-            "map" : mapping,
-            "name" : stream_name,
-            "type" : stream_type,
-            "lang" : lang,
-            "location" : download_location
-        }
-        buttons.append([
-            InlineKeyboardButton(
-                f"{stream_type.upper()} - {str(lang).upper()}", f"{stream_type}_{mapping}_{message.chat.id}-{msg.message_id}"
-            )
-        ])
-
-    buttons.append([
-        InlineKeyboardButton("CANCEL",f"cancel_{mapping}_{message.chat.id}-{msg.message_id}")
-    ])    
-
-    await msg.edit_text(
-        "**Select the Stream to be Extracted...**",
-        reply_markup=InlineKeyboardMarkup(buttons)
-        )
+    await clean_up(dwld_loc)
+    await upload_video(client, message, out_loc)
+    
 
 async def download_url_link(client, message):
     
