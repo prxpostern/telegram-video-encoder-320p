28e5781943bdaabf1a2ed001832e1bbaa2a24f9b
prxpostern <44132439+prxpostern@users.noreply.github.com>
2021-11-27 13:19:05 +0330

    Update upload.py

--- helpers/upload.py
+++ helpers/upload.py
@@ -10,94 +10,51 @@ from hachoir.metadata import extractMetadata
 from helpers.download_from_url import get_size
 from helpers.tools import clean_up
 from helpers.progress import progress_func
-
+from helpers.thumbnail_video import thumb_creator
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
-async def upload_audio(client, message, file_loc):
+async def upload_video(client, message, file_loc):
 
     msg = await message.edit_text(
-        text="**Uploading extracted stream...**",
+        text="**Uploading Video ...**",
         reply_markup=InlineKeyboardMarkup(
             [[InlineKeyboardButton(text="Progress", callback_data="progress_msg")]])
     )
 
-    title = None
-    artist = None
-    thumb = None
     duration = 0
-
     metadata = extractMetadata(createParser(file_loc))
-    if metadata and metadata.has("title"):
-        title = metadata.get("title")
-    if metadata and metadata.has("artist"):
-        artist = metadata.get("artist")
     if metadata and metadata.has("duration"):
         duration = metadata.get("duration").seconds
-    
-    fn = os.path.basename(file_loc)
-    ex = os.path.splitext(fn)[1]
-    fn = os.path.splitext(fn)[0]
-    fn = os.path.splitext(fn)[0]
-    if ex == ".aac" or ex == ".mp3":
-        fn = fn + ex
-    else:
-        fn = fn + ".mka"
+    if metadata and metadata.has("width") and metadata.has("height"):
+        width = metadata.get("width")
+        height = metadata.get("height")
     size = os.path.getsize(file_loc)
     size = get_size(size)
     
+    await msg.edit(f"🏞 Generating thumbnail")
+    thumbnail = await thumb_creator(file_path)
+    
     c_time = time.time()    
     try:
-        await client.send_audio(
+        await client.send_video(
             chat_id=message.chat.id,
-            audio=file_loc,
-            file_name=str(fn),
-            thumb=thumb,
+            video=file_loc,
+            thumb=str(thumbnail),
             caption=f"{fn} [{size}]",
-            title=title,
-            performer=artist,
+            width=width,
+            height=height,
             duration=duration,
             progress=progress_func,
             progress_args=(
-                "**Uploading extracted stream...**",
+                "**Uploading Video:**",
                 msg,
                 c_time
             )
         )
     except Exception as e:
         print(e)     
-        await msg.edit_text("**Some Error Occurred. See Logs for More Info.**")   
-        return
-
-    await msg.delete()
-    await clean_up(file_loc)    
-
-
-async def upload_subtitle(client, message, file_loc):
-
-    msg = await message.edit_text(
-        text="**Uploading extracted subtitle...**",
-        reply_markup=InlineKeyboardMarkup(
-            [[InlineKeyboardButton(text="Progress", callback_data="progress_msg")]])
-    )
-
-    c_time = time.time() 
-
-    try:
-        await client.send_document(
-            chat_id=message.chat.id,
-            document=file_loc,
-            caption="**@posternaudext001bot**",
-            progress=progress_func,
-            progress_args=(
-                "**Uploading extracted subtitle...**",
-                msg,
-                c_time
-            )
-        )
-    except Exception as e:
-        print(e)     
-        await msg.edit_text("**Some Error Occurred. See Logs for More Info.**")   
+        await msg.edit_text(f"**Some Error Occurred.\n\n{e}**")   
         return
 
     await msg.delete()
