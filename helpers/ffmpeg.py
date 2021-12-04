fcea76630fb925ae350eb84042b62480fc1ac38b
prxpostern <44132439+prxpostern@users.noreply.github.com>
2021-12-04 11:03:22 +0330

    Update ffmpeg.py

--- helpers/ffmpeg.py
+++ helpers/ffmpeg.py
@@ -11,13 +11,9 @@ async def enc_video(client, message, data):
     
     await message.edit_text(f"`{dwld_loc}`\n\nEncoding Video ...\n\n**please wait**")
 
-    if os.path.splitext(dwld_loc)[1]:
-        out_loc = os.path.splitext(dwld_loc)[0]
-        out_loc = dwld_loc + "_320p.mp4"
-    else:
-        out_loc = dwld_loc + "_320p.mp4"
+    out_loc = os.path.splitext(dwld_loc)[0]
+    out_loc = out_loc + "_320p.mp4"
     
-
     #out, err, rcode, pid = await execute(f"ffmpeg -i \"{dwld_loc}\" -map 0:{data['map']} -af \"pan=stereo|c0=c0|c1=c0\" -ar 48000 -ab 256k -f mp3 \"{out_loc}\" -y")
     out, err, rcode, pid = await execute(f"ffmpeg -i \"{dwld_loc}\" -c:v libx264 -crf 20 -s 320*240 -c:a aac -ar 48000 -ab 96k \"{out_loc}\" -y")
     if rcode != 0:
