0cc9a09043a064ddbb24838d759bd988f902fb5b
prxpostern <44132439+prxpostern@users.noreply.github.com>
2021-12-04 10:00:35 +0330

    Update ffmpeg.py

--- helpers/ffmpeg.py
+++ helpers/ffmpeg.py
@@ -4,28 +4,33 @@
 from helpers.tools import execute, clean_up
 from helpers.upload import upload_audio, upload_subtitle
 
-async def extract_audio(client, message, data):
-    await message.edit_text("Extracting Stream from file...")
+async def enc_video(client, message, data):
+    
+    await message.edit_text("`Encoding Video ...`\n\n**please wait**")
 
     dwld_loc = data['location']
-    out_loc = data['location'] + ".mp3"
+    
+    if os.path.splitext(dwld_loc)[1]:
+        dwld_loc = os.path.splitext(dwld_loc)[0]
+        out_loc = dwld_loc + "_320p.mp4"
+    else:
+        out_loc = dwld_loc + "_320p.mp4"
+    
 
     #out, err, rcode, pid = await execute(f"ffmpeg -i \"{dwld_loc}\" -map 0:{data['map']} -af \"pan=stereo|c0=c0|c1=c0\" -ar 48000 -ab 256k -f mp3 \"{out_loc}\" -y")
-    out, err, rcode, pid = await execute(f"ffmpeg -i \"{dwld_loc}\" -map 0:{data['map']} -ar 48000 -ab 256k -f mp3 \"{out_loc}\" -y")
+    out, err, rcode, pid = await execute(f"ffmpeg -i \"{dwld_loc}\" -c:v libx264 -crf 20 -s 320*240 -c:a aac -ar 48000 -ab 96k \"{out_loc}\" -y")
     if rcode != 0:
-        await message.edit_text(f"**(mp3 256k) - Error Occured.\n\n{err}**")
+        await message.edit_text(f"**(320p mp4 96k) - Error Occured.\n\n{err}**")
         print(err)
         await clean_up(dwld_loc, out_loc)
         return
-    
 
     await clean_up(dwld_loc)
-    tt = data['title']
-    status = await upload_audio(client, message, out_loc, tt)
+    status = await upload_audio(client, message, out_loc)
     if status:
         time.sleep(3)
-        await upload_audio(client, message, out_loc, tt)
-"""
+        await upload_audio(client, message, out_loc)
+
 async def extract_subtitle(client, message, data):
     await message.edit_text("Extracting Stream from file")
 
@@ -41,4 +46,3 @@ async def extract_subtitle(client, message, data):
 
     await clean_up(dwld_loc)  
     await upload_subtitle(client, message, out_loc)
-    """
